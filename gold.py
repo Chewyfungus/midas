91093b0c18a1a999b122a77fc276deffee3d4654
Ayla Johnson <collywagizzle@gmail.com>
2020-01-18 17:31:14 -0800

    pandas test

--- gold.py
+++ gold.py
@@ -2,6 +2,7 @@ import csv
 import tensorflow as tf
 import numpy as np
 from midas import logger
+import pandas as pd
 
 
 class Gold:
@@ -9,11 +10,25 @@ class Gold:
         logger.debug("Initialized a Gold object, {}".format(repr(self)))
 
     def test(self):
-        with open("data/gold_futures_train.csv", newline='') as csvfile:
-            training_data = csv.reader(csvfile)
-            for row in training_data:
-                print(', '.join(row))
+        train_data = pd.DataFrame(pd.read_csv("data/gold_futures_train.csv"))
 
-        test_file_path = tf.keras.utils.get_file("gold_futures_test.csv")
-        np.set_printoptions(precision=3, suppress=True)
+        """
+        , dtype = {
+            ""
+        }
+        """
 
+        test_data = pd.DataFrame(pd.read_csv("data/gold_futures_test.csv"))
+
+        logger.info(train_data.head())
+
+        p_tr_max = train_data['High'].max()
+        p_tr_min = train_data['Low'].min()
+
+        p_te_max = test_data['High'].max()
+        p_te_min = test_data['Low'].min()
+
+        logger.info(p_tr_max)
+        logger.info(p_tr_min)
+        logger.info(p_te_max)
+        logger.info(p_te_min)
