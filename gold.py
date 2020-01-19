7fc1b7630fd8784c4b6ef5954c24c8afe3043192
Ayla Johnson <collinmaxjohnson@gmail.com>
2020-01-18 20:02:36 -0800

    testing gold price scaler

--- gold.py
+++ gold.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 from midas import logger
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 class Gold:
     def __init__(self):
@@ -34,5 +35,21 @@ class Gold:
         logger.info(train_data.min())
         logger.info(test_data.max())
         # Max: 1613.3; Min: 255
+        xmax = 1613.3
+        xmin = 255.0
 
+        train_data['Price', 'Open'] = pd.apply(lambda x: self.p_scaled(x) if x.name in ['Price', 'Open'] else x)
+        logger.info(train_data.head())
+        # test_data["Price"] = pd.apply(self.p_scaled)
 
+        # modDfObj = dfObj.apply(lambda x: np.square(x) if x.name in ['x', 'y'] else x)
+
+        # X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+        # X_scaled = X_std * (max - min) + min
+
+    def p_std(self, x):
+        return ((x - 255.0) / (1613.3 - 255.0))
+
+    def p_scaled(self, x):
+        scale = (1 / (1613.3 - 255.0)) - 255.0
+        return x
