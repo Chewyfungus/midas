72af55d69a9d527146ff531a11703951cc3f9490
Ayla Johnson <collywagizzle@gmail.com>
2020-01-18 17:31:14 -0800

    pandas test

--- gold.py
+++ gold.py
@@ -2,6 +2,7 @@ import csv
 import tensorflow as tf
 import numpy as np
 from midas import logger
+import pandas as pd
 
 
 class Gold:
@@ -9,11 +10,8 @@ class Gold:
         logger.debug("Initialized a Gold object, {}".format(repr(self)))
 
     def test(self):
-        with open("data/gold_futures_train.csv", newline='') as csvfile:
-            training_data = csv.reader(csvfile)
-            for row in training_data:
-                print(', '.join(row))
+        train_data = pd.read_csv("data/gold_futures_train.csv")
 
-        test_file_path = tf.keras.utils.get_file("gold_futures_test.csv")
-        np.set_printoptions(precision=3, suppress=True)
+        test_data = pd.read_csv("data/gold_futures_test.csv")
 
+        logger.info(train_data.head())
