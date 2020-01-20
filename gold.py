8e39c2b46b0abea026736b9f02ce93aa0f769579
Ayla Johnson <collinmaxjohnson@gmail.com>
2020-01-19 20:59:58 -0800

    concat test

--- gold.py
+++ gold.py
@@ -13,22 +13,23 @@ class Gold:
         train_data = pd.DataFrame(pd.read_csv("data/gold_futures_train.csv",))
         test_data = pd.DataFrame(pd.read_csv("data/gold_futures_test.csv"))
 
-        cols = ["Price", "Open", "High", "Low"]
+        cols0 = ["Price0", "Open0", "High0", "Low0"]
+        cols1 = ["Price1", "Open1", "High1", "Low1"]
 
-        train_data[cols] = train_data[cols].replace({'\$': '', ',': ''}, regex=True)
-        test_data[cols] = test_data[cols].replace({'\$': '', ',': ''}, regex=True)
+        train_data[cols0] = train_data[cols0].replace({'\$': '', ',': ''}, regex=True)
+        test_data[cols1] = test_data[cols1].replace({'\$': '', ',': ''}, regex=True)
 
-        train_data["Date"] = pd.to_datetime(train_data["Date"])
-        train_data["Price"] = pd.to_numeric(train_data["Price"])
-        train_data["Open"] = pd.to_numeric(train_data["Open"])
-        train_data["High"] = pd.to_numeric(train_data["High"])
-        train_data["Low"] = pd.to_numeric(train_data["Low"])
+        train_data["Date0"] = pd.to_datetime(train_data["Date0"])
+        train_data["Price0"] = pd.to_numeric(train_data["Price0"])
+        train_data["Open0"] = pd.to_numeric(train_data["Open0"])
+        train_data["High0"] = pd.to_numeric(train_data["High0"])
+        train_data["Low0"] = pd.to_numeric(train_data["Low0"])
 
-        test_data["Date"] = pd.to_datetime(train_data["Date"])
-        test_data["Price"] = pd.to_numeric(test_data["Price"])
-        test_data["Open"] = pd.to_numeric(test_data["Open"])
-        test_data["High"] = pd.to_numeric(test_data["High"])
-        train_data["Low"] = pd.to_numeric(train_data["Low"])
+        test_data["Date1"] = pd.to_datetime(train_data["Date1"])
+        test_data["Price1"] = pd.to_numeric(test_data["Price1"])
+        test_data["Open1"] = pd.to_numeric(test_data["Open1"])
+        test_data["High1"] = pd.to_numeric(test_data["High1"])
+        train_data["Low1"] = pd.to_numeric(train_data["Low1"])
 
         logger.info(train_data.head())
 
@@ -42,7 +43,7 @@ class Gold:
 
         m_data = pd.concat([train_data, test_data], axis=1)
         m_data.head()
-        m_data[["Price", "Open"]] = scaler.fit_transform(m_data[["Price", "Open"]])
+        m_data[["Price0", "Open0", "Price1", "Open1"]] = scaler.fit_transform(m_data[["Price0", "Open0", "Price1", "Open1"]])
         # m_data["Price"] = train_data.apply(lambda x: self.p_scaled(x) if x.name == "Price" else x)
 
         logger.info(m_data.head())
